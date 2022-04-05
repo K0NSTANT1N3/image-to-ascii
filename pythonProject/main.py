6ae6732281e0be39b2b08a05a7bc783fd3e9a344
SN0K <86849637+K0NSTANT1N3@users.noreply.github.com>
2022-04-05 15:35:01 +0400

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,11 @@
 import PIL.Image
 
-path = 'images/violin-girl.jpg'
+# path = 'images/img.png'
+path = input('Full path of image: ')
 image = PIL.Image.open(path)
 
 USED_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
-new_width = 150
+new_width = int(input('Image width: '))
 
 chara: str = ''.join(USED_CHARS[pixel // 25] for pixel in
                      image.resize((int(new_width), int(new_width * image.height / image.width))).convert("L").getdata())
@@ -14,4 +15,3 @@ ascii_image = "\n".join([chara[index:(index + new_width)] for index in range(0, 
 print(ascii_image)
 with open("ascii_image.txt", "w") as f:
     f.write(ascii_image)
-
