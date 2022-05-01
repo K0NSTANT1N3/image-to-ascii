daed1c9998cbc1af3eab5e0b38a6c5c28bd4dc1d
SN0K <86849637+K0NSTANT1N3@users.noreply.github.com>
2022-05-01 15:10:41 +0400

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,17 +1,21 @@
-import PIL.Image
+import PIL
+from PIL import Image
 
-# path = 'images/img.png'
 path = input('Full path of image: ')
 image = PIL.Image.open(path)
 
 USED_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
-new_width = int(input('Image width: '))
+
+new_height = int(input('Image height: '))
+new_width = int(new_height * image.width / image.height)
 
 chara: str = ''.join(USED_CHARS[pixel // 25] for pixel in
-                     image.resize((int(new_width), int(new_width * image.height / image.width))).convert("L").getdata())
+                     image.resize((int(new_width), int(new_height))).convert("L").getdata())
 
 ascii_image = "\n".join([chara[index:(index + new_width)] for index in range(0, len(chara), new_width)])
 
 print(ascii_image)
 with open("ascii_image.txt", "w") as f:
     f.write(ascii_image)
+
+iss: str
