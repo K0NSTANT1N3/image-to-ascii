78ef3b85b20ce1a0592cb81fa145cbff0e3ad8b6
SN0K <86849637+K0NSTANT1N3@users.noreply.github.com>
2022-12-04 17:45:52 +0400

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,21 +1,22 @@
+import pathlib
 import PIL
 from PIL import Image
 
 path = input('Full path of image: ')
 image = PIL.Image.open(path)
 
-USED_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+USED_CHARS = ["@", "#", "S", "%", "X", "?", "*", "+", ";", ":", ",", ".", " "]
 
 new_height = int(input('Image height: '))
 new_width = int(new_height * image.width / image.height)
 
-chara: str = ''.join(USED_CHARS[pixel // 25] for pixel in
+chara: str = ''.join(USED_CHARS[int(pixel // 19.7)] for pixel in
                      image.resize((int(new_width), int(new_height))).convert("L").getdata())
 
 ascii_image = "\n".join([chara[index:(index + new_width)] for index in range(0, len(chara), new_width)])
 
 print(ascii_image)
-with open("ascii_image.txt", "w") as f:
-    f.write(ascii_image)
 
-iss: str
+prefix = pathlib.PurePath(path).stem
+with open(f"converted/ascii_{prefix}.txt", "w") as f:
+    f.write(ascii_image)
